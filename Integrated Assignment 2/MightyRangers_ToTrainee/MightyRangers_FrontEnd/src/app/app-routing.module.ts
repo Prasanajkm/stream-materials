b73cdf5bbdabbb685c24774ff1c8227613d81728
Prasanajkm <49262801+Prasanajkm@users.noreply.github.com>
2020-03-17 16:28:39 +0530

    Add files via upload

---
+++ Integrated Assignment 2/MightyRangers_ToTrainee/MightyRangers_FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ViewRangerComponent } from './view-ranger/view-ranger.component';
+
+const routes: Routes =
+[
+  { path: '', redirectTo: 'home', pathMatch:"full" },
+  { path: 'home', component: HomeComponent},
+  { path: '**', redirectTo: 'home', pathMatch:"full" }
+];
+
+@NgModule
+({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
